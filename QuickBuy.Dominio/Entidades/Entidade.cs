65c13dc100fd7bbd488b331379425a848ac8aa3b
marcus_vinycius <marcus_vinycius@hotmail.com>
2019-12-15 21:31:26 -0300

    finalizou o curso de numero 21

--- QuickBuy.Dominio/Entidades/Entidade.cs
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -1,7 +1,30 @@
-﻿namespace QuickBuy.Dominio.Entidades
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace QuickBuy.Dominio.Entidades
 {
     public abstract class Entidade
     {
-        public int Id{get; set;}
+        public List<string> _mensagensValidacao { get; set; }
+        private List<string> mensagemValidacao 
+        { 
+            get;
+            //get { return _mensagensValidacao ?? (_mensagensValidacao == new List<string>()); }
+        }
+
+        public abstract void Validade();
+        protected bool EhValidado
+        {
+            get { return !mensagemValidacao.Any(); }
+        }
+
+        protected void LimparMensagemValidacao() {
+            mensagemValidacao.Clear();
+        }
+
+        protected void AdicionarCritica(string mensagem)
+        {
+            mensagemValidacao.Add(mensagem);
+        }
     }
 }
